3af4fbb5b540db0f711135f11255cfee4a2d9fc8
mehrez bey <mahrezbey@gmail.com>
2024-01-17 21:40:14 +0100

    first-commit

---
+++ c/dictionary.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+typedef struct {
+    char* value;
+    Dictionary* swap;
+    Dictionary* next;
+} Dictionary;
+//---- Mehrez
+
+//---- N3dhir
+
+int main(){
+
+
+    return 0;
+}
